96bafdab4fbf303133c49cd01a1ec1d4e26621e1
Bishal <bishaljain18@gmail.com>
2025-07-20 22:28:19 +0530

    api setup

---
+++ coffeemasters-vanilla-main/app.js
@@ -0,0 +1,14 @@
+import Store from './services/Store.js'
+import Router from './services/Router.js';
+import { loadData } from './services/Menu.js'
+
+window.app = {}
+
+app.store = Store;
+app.router = Router;
+
+window.addEventListener("DOMContentLoaded", () => {
+    loadData()
+    app.router.init()
+    console.log(' in the worddddd', app);
+})
---
+++ coffeemasters-vanilla-main/services/Router.js
@@ -0,0 +1,53 @@
+const Router = {
+    init: () => {
+        document.querySelectorAll('a.navlink').forEach(link => {
+            link.addEventListener('click', (event) => {
+                event.preventDefault()
+                const url = event.target.getAttribute('href')
+                Router.go(url)
+            })
+        })
+        //Check the initial URL
+        Router.go(location.pathname)
+        window.addEventListener("popstate", event => {
+            Router.go(event.state.route, false)
+        })
+
+    },
+    go: (route, addToHistory = true) => {
+        // Implement navigation logic here
+        console.log('going to', route)
+
+        if (addToHistory) {
+            history.pushState({ route }, '', route)
+        }
+        let pageElement = null;
+        switch(route) {
+            case "/":
+                pageElement = document.createElement("h1");
+                pageElement.textContent = "Menu"
+                break;
+            case "/order":
+                pageElement = document.createElement("h1");
+                pageElement.textContent = "Your Order"
+                break;
+            default:
+                if(route.startsWith("product-")) {
+                    pageElement = document.createElement("h1")
+                    pageElement.textContent = "Details"
+                    const paramID = route.subString(route.lastIndexOf("-")+1)
+                    pageElement.dataset.id = paramID
+                }
+        }
+        if(pageElement) {
+
+            const cache = document.querySelector("main");
+            cache.innerHTML = ""
+            document.querySelector("main").appendChild(pageElement)
+            window.scrollX = 0
+            window.scrollY = 0
+        }
+    }
+}
+
+export default Router
