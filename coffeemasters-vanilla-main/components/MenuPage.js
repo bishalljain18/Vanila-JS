a0869d300577dd40cf2423667fe4e6c31ee4dc56
Bishal <bishaljain18@gmail.com>
2025-07-21 23:42:48 +0530

    add menu

--- coffeemasters-vanilla-main/components/MenuPage.js
+++ coffeemasters-vanilla-main/components/MenuPage.js
@@ -1,6 +1,43 @@
 export class MenuPage extends HTMLElement {
     constructor() {
         super()
+        this.root = this.attachShadow({ mode: 'open' })
+
+        const styles = document.createElement("style")
+        this.root.appendChild(styles)
+        async function loadCSS() {
+            const request = await fetch("/components/MenuPage.css")
+            const css = await request.text();
+            styles.textContent = css
+        }
+        loadCSS()
+    }
+    connectedCallback() {
+        const template = document.getElementById('menu-page-template')
+        const content = template.content.cloneNode(true);
+        this.root.appendChild(content)
+
+        window.addEventListener('appmenuchange', () => {
+            this.render()
+        })
+    }
+    render() {
+        const menu = app.store.menu;
+        console.log('menu', menu)
+        
+        if(menu) {
+            for(let category of menu) {
+                const liCategory = document.createElement('li');
+                liCategory.innerHTML = `
+                <h3>${category.name}</h3>
+                <ul class='category'></ul>
+                `;
+                this.root.querySelector('#menu').appendChild(liCategory)
+                
+            }
+        } else {
+            this.root.querySelector("#menu").innerHTML = "Loading..."
+        }
     }
 }
 
